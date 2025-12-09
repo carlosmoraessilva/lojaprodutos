f0bf99d8b0d31b72e95ad6b01cbcda0ac1a16102
Carlos Moraes <carlosm1412@gmail.com>
2025-12-08 21:49:48 -0300

    Criação do projeto e do banco de dados

---
+++ LojaProdutos/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaProdutos.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ LojaProdutos/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LojaProdutos.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+                
+        }
+    }
+}
