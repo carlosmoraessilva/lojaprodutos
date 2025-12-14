07848f6108268b4258e2db66e29ad64dabc54441
Carlos Moraes <carlosm1412@gmail.com>
2025-12-13 21:02:50 -0300

    View Cadastrar

--- LojaProdutos/obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
+++ LojaProdutos/obj/Debug/net8.0/LojaProdutos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12d7913df41191d4a137f3123a731800bb0aa57a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+225ae1d28e9f5fc83688bb9e8b0cf67ca85f7ebe")]
 [assembly: System.Reflection.AssemblyProductAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LojaProdutos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
